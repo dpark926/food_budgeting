3dd29e288929a3c2f122c123ba3935b701574bea
Daniel Park <dpeace926@gmail.com>
2019-05-06 18:26:26 -0400

    initialize next app

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style>{`body { margin: 0 } /* custom! */`}</style>
+          <link
+            href="https://unpkg.com/basscss@8.0.2/css/basscss.min.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
