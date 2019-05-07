eb5ef956d4e8fd5524d4bf26589afcf53ee44b17
Daniel Park <dpeace926@gmail.com>
2019-05-06 20:45:04 -0400

    install sass, rechart

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home() {
-  return <div>Welcome to Next.js!</div>;
+function index() {
+  return (
+    <div>
+      <form>
+        <input
+          type="number"
+          min="0.01"
+          step="0.01"
+          max="2500"
+          placeholder="25.67"
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
-export default Home;
+export default index;
