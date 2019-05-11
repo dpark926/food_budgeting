d0f206f9a1ff10ecc68626622d8fbd9c93a13c2c
Daniel Park <dpeace926@gmail.com>
2019-05-10 22:45:58 -0400

    install next-images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 const withSass = require("@zeit/next-sass");
+const withImages = require("next-images");
 
 const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack");
 
-module.exports = withSass({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+module.exports = withSass(
+  withImages({
+    webpack(config) {
+      config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
-    return config;
-  }
-});
+      return config;
+    }
+  })
+);
