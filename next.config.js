fc8cba65e53fad9d8ddcba8e08ae31cedca64ab1
Daniel Park <dpeace926@gmail.com>
2019-05-11 00:15:21 -0400

    try

--- next.config.js
+++ next.config.js
@@ -4,10 +4,23 @@ const withImages = require("next-images");
 const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack");
 
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
 module.exports = withSass(
   withImages({
-    webpack(config) {
-      config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    webpack: config => {
+      config.plugins = config.plugins || [];
+
+      config.plugins = [
+        ...config.plugins,
+
+        // Read the .env file
+        new Dotenv({
+          path: path.join(__dirname, ".env"),
+          systemvars: true
+        })
+      ];
 
       return config;
     }
