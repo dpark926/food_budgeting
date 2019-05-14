0b95eaa4f90f7a8cc649228c89dd5a614d676d98
Daniel Park <dpeace926@gmail.com>
2019-05-14 14:17:29 -0400

    dotenv-webpack path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,33 @@
 const withSass = require("@zeit/next-sass");
-const { parsed: localEnv } = require("dotenv").config();
-const webpack = require("webpack");
+// const { parsed: localEnv } = require("dotenv").config();
+// const webpack = require("webpack");
+//
+// module.exports = withSass({
+//   webpack(config) {
+//     config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+//
+//     return config;
+//   }
+// });
+
+require("dotenv").config();
+
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = withSass({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+  webpack: config => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, ".env"),
+        systemvars: true
+      })
+    ];
 
     return config;
   }
