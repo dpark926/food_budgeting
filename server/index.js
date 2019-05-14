3000f6a9f0a3c916db22b02cc1b659f40fc31c7a
Daniel Park <dpeace926@gmail.com>
2019-05-14 16:43:47 -0400

    yo

--- server/index.js
+++ server/index.js
@@ -24,6 +24,15 @@ nextApp.prepare().then(() => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/api/transactions", transactions);
+  // Serve static assets if in production
+  if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
   app.get("*", (req, res) => {
     return handle(req, res); // for all the react stuff
   });
