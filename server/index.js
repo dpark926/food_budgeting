5252ed7003ab5537b0237ad0648b92ea562ed6ae
Daniel Park <dpeace926@gmail.com>
2019-05-14 19:47:49 -0400

    revert

--- server/index.js
+++ server/index.js
@@ -10,12 +10,6 @@ const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler(); //part of next config
 const mongoose = require("mongoose");
 
-const routes = require("./routes");
-const routerHandler = routes.getRequestHandler(nextApp);
-const glob = require("glob");
-
-const { config } = require("../config/config");
-
 nextApp.prepare().then(() => {
   const app = express();
   const db = mongoose
@@ -38,27 +32,11 @@ nextApp.prepare().then(() => {
   });
 
   app.use("/api/transactions", transactions);
-  // app.get("*", (req, res) => {
-  //   return handle(req, res); // for all the react stuff
-  // });
-  // app.listen(PORT, err => {
-  //   if (err) throw err;
-  //   console.log(`ready at http://localhost:${PORT}`);
-  // });
-
-  // REST API routes
-  // const rootPath = require("path").join(__dirname, "/..");
-  // glob.sync(rootPath + "/server/routes/api/*.js").forEach(controllerPath => {
-  //   if (!controllerPath.includes(".test.js")) require(controllerPath)(app);
-  // });
-
-  // Next.js page routes
-  app.get("*", routerHandler);
-
-  // Start server
-  app.listen(config.serverPort, () =>
-    console.log(
-      `${config.appName} running on http://localhost:${config.serverPort}/`
-    )
-  );
+  app.get("*", (req, res) => {
+    return handle(req, res); // for all the react stuff
+  });
+  app.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`ready at http://localhost:${PORT}`);
+  });
 });
