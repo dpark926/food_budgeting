097025f193ce8a390d3f1c15132084820aac658a
Daniel Park <dpeace926@gmail.com>
2019-05-16 13:39:24 -0400

    test

--- server/index.js
+++ server/index.js
@@ -10,38 +10,35 @@ const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler(); //part of next config
 const mongoose = require("mongoose");
 
-nextApp
-  .prepare()
-  .then(() => {
-    const app = express();
-    // const db = mongoose
-    //   .connect(
-    //     process.env.MONGODB_URI,
-    //     { useNewUrlParser: true }
-    //   )
-    //   .then(() => console.log("MongoDB Connected"))
-    //   .catch(err => console.log(err));
-    // app.use(cors());
-    // app.use(bodyParser.json());
-    // app.use(bodyParser.urlencoded({ extended: true }));
-    // app.use(function(req, res, next) {
-    //   res.header("Access-Control-Allow-Origin", "*");
-    //   res.header(
-    //     "Access-Control-Allow-Headers",
-    //     "Origin, X-Requested-With, Content-Type, Accept"
-    //   );
-    //   next();
-    // });
-    app.get("/a", (req, res) => {
-      return nextApp.render(req, res, "/a", req.query);
-    });
-    app.use("/api/transactions", transactions);
-    app.get("*", (req, res) => {
-      return handle(req, res); // for all the react stuff
-    });
-    app.listen(PORT, err => {
-      if (err) throw err;
-      console.log(`ready at http://localhost:${PORT}`);
-    });
-  })
-  .catch(err => new Error("Oooh noooo c!!!!"));
+nextApp.prepare().then(() => {
+  const app = express();
+  const db = mongoose
+    .connect(
+      process.env.MONGODB_URI,
+      { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  app.use("/api/transactions", transactions);
+  app.get("/a", (req, res) => {
+    return res.end("We made it!");
+  });
+  app.get("*", (req, res) => {
+    return handle(req, res); // for all the react stuff
+  });
+  app.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`ready at http://localhost:${PORT}`);
+  });
+});
